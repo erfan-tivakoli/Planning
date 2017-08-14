0c5a3b6f21d4dbc370eb359c7871cd8be8a5eaa3
Erfan Tavakoli <erfan.tavakoli71@gmail.com>
2017-08-14 22:43:45 +0430

    almost completed parsing

--- parser.py
+++ parser.py
@@ -1,6 +1,19 @@
+from Planning.operator import operators_parser
+from Planning.predicate import predicates_parser
+
 
 def parse(file_path):
     with open(file_path, 'r') as f:
         lines = f.readlines()
-        for line in lines:
+        counter = 0
+        num_of_predicates = int(lines[counter].split(":")[1])
+        predicates_parser(lines[:num_of_predicates + 1])
+
+        counter += num_of_predicates + 1
+
+        counter += 2
+
+        counter += 1
+
+        operators_parser(lines[counter:])
             
