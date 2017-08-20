93cc501a181c335ff744b6d41ba54a63e0cf84b6
Chester <chester@Chesters-MacBook-Pro.local>
2017-08-20 11:41:00 +0430

    delta 2 is better

--- hsp.py
+++ hsp.py
@@ -17,8 +17,11 @@ def HSP(plan, state, goals, actions, predicates):
             res = delta(next_s, predicates, actions)
             options.append((action, res, next_s))
     while len(options) > 0:
-        action_index = select_action(options, goals)
+        action_index, action_cost = select_action(options, goals)
         option = options.pop(action_index)
+        print "action : " + str(option[0][0].name) + str(option[0][1]) + ", cost : " +\
+              str(action_cost) + ", state : " +\
+              "  " + " ".join(str(predicate) + " " for predicate in state.predicates.values())
         partial_plan = []
         partial_plan.extend(plan)
         partial_plan.append(option[0])
@@ -33,10 +36,10 @@ def select_action(options, goals):
     min_value = max_pairs(goals.predicates, options[0][1])
     for i in range(1, len(options)):
         temp = max_pairs(goals.predicates, options[i][1])
-        if min_value > temp:
+        if temp < min_value:
             min_value = temp
             min_index = i
-    return min_index
+    return min_index, min_value
 
 
 def next_state(state, action):
@@ -49,8 +52,6 @@ def next_state(state, action):
 
 
 def delta(s, all_ground_predicates, all_ground_operators):
-    print("delta")
-
     res = dict()
     for p in all_ground_predicates:
         for q in all_ground_predicates:
@@ -69,11 +70,20 @@ def delta(s, all_ground_predicates, all_ground_operators):
             if len(pre_conds) > 0:  # Check if operation is applicable on U
                 effects = get_added_effects(op)
                 U.add_predicates(effects)
-                for p in effects:
+                hash_effects = dict()
+                for effect in effects:
+                    hash_effects[hash_predicate(effect)] = effect
+                for p in hash_effects:
                     for q in U.predicates:
-                        key = hash_predicates(hash_predicate(p), q)
+                        key = hash_predicates(p, q)
                         last_value = res[key]
-                        new_value = max_pairs(pre_conds, res) + 1
+                        requirements = dict()
+                        requirements.update(pre_conds)
+                        if q in hash_effects:
+                            new_value = max_pairs(requirements, res) + 1
+                        else:
+                            requirements[q] = U.predicates[q]
+                            new_value = max_pairs(requirements, res) + 1
                         if last_value > new_value:
                             res[key] = new_value
                             change = True
